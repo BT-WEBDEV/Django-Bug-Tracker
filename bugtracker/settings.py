2c186a4b00e3ed16b606f6e052c1c3d9778fb479
Brandon Tetrick <brandontetrick@gmail.com>
2020-08-25 14:53:30 -0600

    Settings

--- bugtracker/settings.py
+++ bugtracker/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'w)r+e!yucry^-9*1bii)a*x5u9mo%x08bg!h&yv6m^q6dsi)f^'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG=True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['bt-bug-tracker.herokuapp.com']
 
 
 # Application definition
